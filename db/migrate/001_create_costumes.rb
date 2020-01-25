ed352abd4586da3470c2820b571c66c32f6a10e8
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-25 17:44:03 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,9 +11,10 @@ class CreateCostumes << ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :price
-      t.string :size 
+      t.integer :price
+      t.integer :size 
       t.string :image_url
+      t. timestamps
     end
   end
   
