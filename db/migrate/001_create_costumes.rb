975fecfb7b8237958c0b24ecbc98fc96703cbc08
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-25 17:44:26 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes << ActiveRecord::Migration[4.2]
       t.integer :price
       t.integer :size 
       t.string :image_url
-      t. timestamps
+      t.timestamps
     end
   end
   
