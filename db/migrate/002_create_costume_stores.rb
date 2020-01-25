a5b97acb3135080e373522ab054d105a6c091a0f
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-25 17:46:07 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size 
+      t.string :image_url
+      t.timestamps
+    end
+  end
+  
+end
