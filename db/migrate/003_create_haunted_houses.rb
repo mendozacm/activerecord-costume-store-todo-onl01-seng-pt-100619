fc370ed35f5ec7d17a029024d84fa45c05dd7b47
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-25 18:01:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table :costumes do |t|
+    create_table :haunted_house do |t|
       t.string :name
       t.integer :price
       t.integer :size 
