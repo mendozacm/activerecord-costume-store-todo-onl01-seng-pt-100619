918508d02187e4d7ddad9b70b146471509002da5
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-25 18:00:13 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.integer :still_in_business
+      t.string :still_in_business
       t.integer :opening_time
       t.integer :closing_time
     end
